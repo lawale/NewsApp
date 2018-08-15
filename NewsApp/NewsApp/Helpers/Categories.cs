a56e1dbd9bf409245b47d7b7b68869f959b0a5e6
Olawale Lawal <lawale99@gmail.com>
2018-08-15 01:08:03 +0100

    Creating the helper classes and the new model class

---
+++ NewsApp/NewsApp/Helpers/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsApp.Helpers
+{
+    public static class Categories
+    {
+        public static readonly string business = "business";
+        public static readonly string entertainment = "entertainment";
+        public static readonly string health = "health";
+        public static readonly string science = "science";
+        public static readonly string sports = "sports";
+        public static readonly string technology = "technology";
+    }
+}
---
+++ NewsApp/NewsApp/Helpers/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsApp.Helpers
+{
+    public class Constants
+    {
+        private const string HeadLines = "https://newsapi.org/v2/top-headlines?country=ng&apiKey={0}";
+        private const string Others = "https://newsapi.org/v2/top-headlines?country=ng&category={0}&apiKey={1}";
+        private const string APiKey = "9b11455b30834425a6c30b0770afd7fd";
+        public string TopStories
+        {
+            get
+            {
+                return string.Format(HeadLines, APiKey);
+            }
+        }
+
+        public string CategoryStories(string cat)
+        {
+            return string.Format(Others, cat, APiKey);
+        }
+    }
+}
