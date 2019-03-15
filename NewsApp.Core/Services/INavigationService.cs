62adfef7b3e632724856246f4ae483d305228ae3
Olawale Lawal <lawale99@gmail.com>
2019-03-15 18:18:07 +0100

    Added the INavigationService interface and NaviagtionService class

---
+++ NewsApp.Core/Services/INavigationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewsApp.Core.Services
+{
+    public interface INavigationService 
+    {
+        Task<Page> PopAsync();
+        Task<Page> PopModalAsync();
+        Task PopToRootAsync();
+        Task PushAsync(Page page);
+        Task PushModalAsync(Page page);
+    }
+}
---
+++ NewsApp.Core/Services/NavigationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using NewsApp.Core.Services;
+using System.Threading.Tasks;
+
+[assembly: Dependency(typeof(NavigationService))]
+namespace NewsApp.Core.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public Task<Page> PopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Page> PopModalAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PopToRootAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PushAsync(Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PushModalAsync(Page page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
