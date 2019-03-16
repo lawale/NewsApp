633efc102c7bc6b817801a4a8b2a61fc0371f929
Olawale Lawal <lawale99@gmail.com>
2019-03-16 01:37:24 +0100

    Implemented the VIewFactory class

---
+++ NewsApp.Core/Factories/IViewFactory.cs
@@ -0,0 +1,19 @@
+﻿using NewsApp.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NewsApp.Core.Factories
+{
+    public interface IViewFactory
+    {
+        void Register<TViewModel, TView>() where TViewModel : class, IViewModel where TView : Page;
+
+        Page Resolve<TViewModel>(Action<TViewModel> setStateAction = null) where TViewModel : class, IViewModel;
+
+        Page Resolve<TViewModel>(out TViewModel viewModel, Action<TViewModel> setStateAction = null) where TViewModel : class, IViewModel;
+
+        Page Resolve<TViewModel>(TViewModel viewModel) where TViewModel : class, IViewModel;
+    }
+}
---
+++ NewsApp.Core/Factories/ViewFactory.cs
@@ -0,0 +1,42 @@
+﻿using NewsApp.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NewsApp.Core.Factories;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ViewFactory))]
+namespace NewsApp.Core.Factories
+{
+    public class ViewFactory : IViewFactory
+    {
+        readonly IDictionary<Type, Type> map = new Dictionary<Type, Type>();
+
+        public void Register<TViewModel, TView>() 
+            where TViewModel : class, IViewModel where TView : Page
+            => map[typeof(TViewModel)] = typeof(TView);
+
+        public Page Resolve<TViewModel>(Action<TViewModel> setStateAction)
+            where TViewModel : class, IViewModel
+            => Resolve(out TViewModel viewModel, setStateAction);
+
+        public Page Resolve<TViewModel>(out TViewModel viewModel, Action<TViewModel> setStateAction) where TViewModel : class, IViewModel
+        {
+            viewModel = DependencyService.Resolve<TViewModel>();
+            var viewType = map[typeof(TViewModel)];
+            var view = Activator.CreateInstance(viewType) as Page;
+            if (setStateAction != null)
+                viewModel.SetState(setStateAction);
+            view.BindingContext = viewModel;
+            return view;
+        }
+
+        public Page Resolve<TViewModel>(TViewModel viewModel) where TViewModel : class,IViewModel
+        {
+            var viewType = map[typeof(TViewModel)];
+            var view = Activator.CreateInstance(viewType) as Page;
+            view.BindingContext = viewModel;
+            return view;
+        }
+    }
+}
